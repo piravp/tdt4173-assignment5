ba804cd6c964295b374ec059ebda27fbc91ce445
vegahas <vegahas@gmail.com>
2018-05-04 11:07:07 +0200

    read files

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import os
+import numpy as np
+from PIL import Image
 
 PATH = os.path.dirname(os.path.abspath(__file__))
-FOLDER = '\chars74k-lite'
+FOLDER = '/chars74k-lite'
 
 FULL_PATH = PATH + FOLDER
 
-print(FULL_PATH)
+images = dict()
+
+first = True
+for folder in os.walk(FULL_PATH):
+    letter_path = folder[0]
+    letter = letter_path[-1]
+    if first:
+        first = False
+        continue
+    idx = 0
+    letter_dict = dict()
+    for file in os.listdir(letter_path):
+        if file.endswith(".jpg"):
+            image = Image.open(letter_path + '/' + file)
+            pix = np.array(image)
+            letter_dict[idx] = pix
+            idx += 1
+    images[letter] = letter_dict
+
 
---
+++ program.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  4 10:08:34 2018
+
+@author: Vegard, Piraveen
+"""
+
+from PIL import Image
+import os
+
+PATH = 3999393;
+
+images = dict()
+
+for folder in os.walk(PATH):
+    letter = folder[0]
+    
+
+print(999)
+
+
